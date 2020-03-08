1a6c053b1ca797b30419ccecfba5c74b9081f548
Aaron <lordgankoo@gmail.com>
2020-03-08 00:38:32 -0600

    Re-integrated release 2 navigation features
    TODO: More thorough testing

--- app/src/main/java/com/suas/MissionControlActivity.java
+++ app/src/main/java/com/suas/MissionControlActivity.java
@@ -3,12 +3,25 @@ package com.suas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MissionControlActivity extends AppCompatActivity {
+
+    private Navigator nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission_control);
+
+        nav = new Navigator();
+    }
+
+    public void startMission(View v) {
+        nav.begin();
+    }
+
+    public void emergencyLanding(View v) {
+        nav.autoLand();
     }
 }
