907d4d2c794188baf6fb2aff9bc6d337e7bd45ff
marknilov <marknilov@yahoo.com>
2020-04-14 19:36:01 -0600

    first working algorithm
    From my testing this will solve every maze configuration without crashing however it is not optimized and might be able to do better on certain configurations if it acted smarter. Beats all configurations in 10-20 moves.

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -1,5 +1,7 @@
 package solver;
 
+import javax.swing.SortOrder;
+
 import maze.Direction;
 import maze.Maze;
 
@@ -11,7 +13,94 @@ public class Solver {
 	}
 	
 	public void solveMaze() {
+		int count = 0;
+		int totalMoves = 0;
+	
+		System.out.println(maze.toString());
+		//starting on the right wall causes problems sometimes,
+		//move left one square
+		while(maze.getDroneCurrentCoordinates().getX() > 2){
+			maze.turn(Direction.WEST);
+			maze.moveFoward();
+			System.out.println(maze.toString());
+			totalMoves++;
+			maze.turn(Direction.NORTH);
+		}
+		
+		while(count<7){
+		//nothing in front, go forward
+		if(maze.isObjectDetected()==false){
+			maze.moveFoward();
+			System.out.println(maze.toString());
+			totalMoves++;
+			count++;
+		}
+		else{
+			//try to go right
+			maze.turn(Direction.EAST);
+			if(maze.isObjectDetected()==false){
+			maze.moveFoward();
+			System.out.println(maze.toString());
+			totalMoves++;
+			}
+			//try to go left
+			else{
+				maze.turn(Direction.WEST);
+				if(maze.isObjectDetected()==false){
+				maze.moveFoward();
+				System.out.println(maze.toString());
+				totalMoves++;
+				}
+				//turn around and go down 2 right 2
+				else{
+					maze.turn(Direction.SOUTH);
+					maze.moveFoward();
+					System.out.println(maze.toString());
+					totalMoves++;
 
+					maze.turn(Direction.EAST);
+					maze.moveFoward();
+					System.out.println(maze.toString());
+					totalMoves++;
+
+					maze.turn(Direction.SOUTH);
+					maze.moveFoward();
+					System.out.println(maze.toString());
+					totalMoves++;
+
+					maze.turn(Direction.EAST);
+					maze.moveFoward();
+					System.out.println(maze.toString());
+					totalMoves++;
+
+					count--;
+					count--;
+				}
+			}
+			maze.turn(Direction.NORTH);
+		}
+		
+		}
+		System.out.println(maze.toString());
+		
+		//Drone is in the last row, find goal
+		while(maze.getGoalCoordinates().getX()<
+		maze.getDroneCurrentCoordinates().getX()){
+			maze.turn(Direction.WEST);
+			maze.moveFoward();
+			System.out.println(maze.toString());
+			totalMoves++;
+		}
+		while(maze.getGoalCoordinates().getX()>
+		maze.getDroneCurrentCoordinates().getX()){
+			maze.turn(Direction.EAST);
+			maze.moveFoward();
+			System.out.println(maze.toString());
+			totalMoves++;
+		}
+
+		System.out.println("Goal Found!");
+		System.out.println("Total Moves: "+ totalMoves);
 	}
 
 }
